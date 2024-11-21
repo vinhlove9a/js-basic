d7638ed51dcfe2f9163c8c81b10506ee2ed0e7e5
vinhlove9a <beochy204@gmail.com>
2024-11-21 10:47:38 +0700

    Vinh commit

---
+++ js/multi-choice-question.js
@@ -0,0 +1,10 @@
+class MultiChoiceQuestion extends Question {
+  constructor(q) {
+    super(q);
+  }
+
+  // Problem 2: Implement code here
+  render() {
+    
+  }
+}
---
+++ js/question.js
@@ -0,0 +1,12 @@
+class Question {
+  constructor(q, index) {
+    this.question = q.question;
+    this.answers = q.answers;
+    this.correctAnswer = q.correctAnswer;
+    this.index = index;
+  }
+
+  render() {
+    throw new Error('Subclass must override this method');
+  }
+}
